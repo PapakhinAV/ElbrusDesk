4929c61f9c78acb39050c0fae7e507195c2eea4e
Alexander <alexander7-93@yandex.ru>
2020-12-17 17:23:31 +0300

    добавил домашнюю страницу

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,27 @@
 import './index.css';
+import UserMenu from "../UserMenu/UserMenu";
+import Post from "../Post/Post"
+import Wall from "../Wall/Wall"
 
-// const HomePage = () => {
-//   return (  );
-// }
+const HomePage = () => {
+  return (
+    <>
 
-// export default HomePage;
+      <div className="MainBlock">
+
+        <div className="leftColumn">
+          <div className="fotoBlock">foto</div>
+          <div className="userMenuBlock"><UserMenu /></div>
+        </div>
+
+        <div className="rightColumn">
+          <div className="newPostBlock"><Post /></div>
+          <div className="wallBlock"><Wall /></div>
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
