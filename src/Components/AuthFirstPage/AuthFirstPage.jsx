156ef6cc9fcb997bbb9e9024fc42be9a61eda618
Salman <osmanov_s.o@mail.ru>
2020-12-21 16:38:39 +0300

    Добавили иконку для google регистрации

--- src/Components/AuthFirstPage/AuthFirstPage.jsx
+++ src/Components/AuthFirstPage/AuthFirstPage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import SignIn from "../SignIn/SignIn"
 import Logo from '../img/EBD.svg'
 import Git from '../img/git-icon.svg'
+import Google from '../img/google-icon.svg'
 
 
 const AuthFirstPage = () => {
@@ -27,14 +28,14 @@ const AuthFirstPage = () => {
                 
               </div>
               <Link className="signUpButton" to="/SignUp">Создать аккаунт</Link>
-<div className="regMain">
-  <div>
-  <a href="http://localhost:3000/auth/github"><img src={Git} alt="git-icon"/></a>
-  </div>
-  <div>
-  <a href="http://localhost:3000/auth/google"><img src={Git} alt="git-icon"/></a>
-  </div>
-</div>
+            <div className="regMain">
+              <div>
+              <a href="http://localhost:3000/auth/google"><img src={Google} alt="git-icon"/></a>
+              </div>
+              <div>
+              <a href="http://localhost:3000/auth/github"><img src={Git} alt="git-icon"/></a>
+              </div>
+            </div>
             </div>
           </div>
 
