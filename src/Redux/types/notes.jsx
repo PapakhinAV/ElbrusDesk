cfb1ec8b7d0af00ae98944e471b76f2342716d23
Alexander <alexander7-93@yandex.ru>
2020-12-21 11:39:23 +0300

    страница администратора в работе

--- src/Redux/types/notes.jsx
+++ src/Redux/types/notes.jsx
@@ -3,3 +3,4 @@ export const smth = 'smth';
 export const ADD_GROUPS = 'ADD_GROUPS';
 export const ADD_USERS_LIST = 'ADD_USERS_LIST';
 export const ADD_USER_ID = "ADD_USER_ID"
+export const ADD_INFO_FOR_ADMIN = "ADD_INFO_FOR_ADMIN"
