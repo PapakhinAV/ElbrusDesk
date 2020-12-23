af06ba5976ab686b3d644fad48541728d0bf5022
Alexander <alexander7-93@yandex.ru>
2020-12-23 16:14:03 +0300

    настроил ссылки на соцсети

--- backend/app.js
+++ backend/app.js
@@ -282,18 +282,20 @@ app.post('/Edit/:id', async (req, res) => {
     gitHub,
     linkidIn,
     instagram,
-		vk, selectIdGroup, selectIdDelete } = req.body
+    email,
+    vk, selectIdGroup, selectIdDelete } = req.body
 
   if (firstname ||
     surname ||
+    email ||
     tel ||
     city ||
     telegram ||
     gitHub ||
     linkidIn ||
     instagram ||
-		vk || 
-		selectIdGroup || selectIdDelete) {
+    vk ||
+    selectIdGroup || selectIdDelete) {
     if (firstname) {
       await User.findByIdAndUpdate(idUserEdit, { firstname: firstname }, function (err, firstname) {
         res.status(200)
@@ -303,9 +305,17 @@ app.post('/Edit/:id', async (req, res) => {
       await User.findByIdAndUpdate(idUserEdit, { surname: surname }, function (err, surname) {
         res.status(200)
       })
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { surname: "" }, function (err, surname) {
+        res.status(200)
+      })
     }
     if (tel) {
       await User.findByIdAndUpdate(idUserEdit, { tel: tel }, function (err, tel) {
+        res.status(200)
+      })
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { tel: "" }, function (err, tel) {
         res.status(200)
       })
     }
@@ -313,32 +323,67 @@ app.post('/Edit/:id', async (req, res) => {
       await User.findByIdAndUpdate(idUserEdit, { city: city }, function (err, city) {
         res.status(200)
       })
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { city: "" }, function (err, city) {
+        res.status(200)
+      })
     }
-    if (telegram || linkidIn || instagram || vk) {
-      if (telegram) {
-				await User.findByIdAndUpdate(idUserEdit, { telegram: telegram }, function (err, telegram) {
-					res.status(200)
-				})
-      }
-      if (linkidIn) {
-        await User.findByIdAndUpdate(idUserEdit, { linkidIn: linkidIn }, function (err, linkidIn) {
-					res.status(200)
-				})
-      }
-      if (instagram) {
-				await User.findByIdAndUpdate(idUserEdit, { instagram: instagram }, function (err, instagram) {
-					res.status(200)
-				})      }
-      if (vk) {
-				await User.findByIdAndUpdate(idUserEdit, { vk: vk }, function (err, vk) {
-					res.status(200)
-				})      }
+
+    if (telegram) {
+      await User.findByIdAndUpdate(idUserEdit, { telegram: telegram }, function (err, telegram) {
+        res.status(200)
+      })
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { telegram: "" }, function (err, telegram) {
+        res.status(200)
+      })
     }
+    if (linkidIn) {
+      await User.findByIdAndUpdate(idUserEdit, { linkidIn: linkidIn }, function (err, linkidIn) {
+        res.status(200)
+      })
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { linkidIn: "" }, function (err, linkidIn) {
+        res.status(200)
+      })
+    }
+    if (instagram) {
+      await User.findByIdAndUpdate(idUserEdit, { instagram: instagram }, function (err, instagram) {
+        res.status(200)
+      })
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { instagram: "" }, function (err, instagram) {
+        res.status(200)
+      })
+    }
+    if (vk) {
+      await User.findByIdAndUpdate(idUserEdit, { vk: vk }, function (err, vk) {
+        res.status(200)
+      })
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { vk: "" }, function (err, vk) {
+        res.status(200)
+      })
+    }
+
     if (gitHub) {
       await User.findByIdAndUpdate(idUserEdit, { gitHub: gitHub }, function (err, gitHub) {
         res.status(200)
       })
-		}
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { gitHub: "" }, function (err, gitHub) {
+        res.status(200)
+      })
+    }
+    if (email) {
+      await User.findByIdAndUpdate(idUserEdit, { email: email }, function (err, email) {
+        res.status(200)
+      })
+    } else {
+      await User.findByIdAndUpdate(idUserEdit, { email: "" }, function (err, gitHub) {
+        res.status(200)
+      })
+    }
     if (selectIdGroup) {
       selectIdGroup.map(el => {
         if (!userOne.stydyGroup.includes(el.value)) {
@@ -346,14 +391,14 @@ app.post('/Edit/:id', async (req, res) => {
         }
       })
       await User.findByIdAndUpdate(idUserEdit, { stydyGroup: userOne.stydyGroup })
-		}
-	// 	if (selectIdDelete) {
-	// 		userOne.stydyGroup.map(el=> (
-	// 			selectIdDelete.map(item =>( item))
-  // ))
+    }
+    // 	if (selectIdDelete) {
+    // 		userOne.stydyGroup.map(el=> (
+    // 			selectIdDelete.map(item =>( item))
+    // ))
     //   await User.findByIdAndUpdate(idUserEdit, { stydyGroup: userOne.stydyGroup })
     // }
-  return res.sendStatus(200)
+    return res.sendStatus(200)
   }
   return res.sendStatus(406)
 })
