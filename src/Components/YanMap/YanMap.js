8fcb1e3d108dfda66af0a7a97192780973a02f51
Alexander <alexander7-93@yandex.ru>
2020-12-24 17:30:43 +0300

    после слияния

--- src/Components/YanMap/YanMap.js
+++ src/Components/YanMap/YanMap.js
@@ -5,7 +5,7 @@ import { Map, Marker, MarkerLayout } from 'yandex-map-react';
 export default function ContactMap({ positions }) {
 
   return (
-    <Map onAPIAvailable={function () { console.log('API loaded'); }} center={[55.754734, 37.583314]} zoom={10}>
+    <Map onAPIAvailable={function () { console.log('API loaded'); }} center={[55.754734, 37.583314]} zoom={10} controls={['zoomControl', 'fullscreenControl']}>
       {positions.map((element) => <Marker lat={element.lat} lon={element.lon}>
         <MarkerLayout>
           <div style={{ borderRadius: '20px', padding: "5px", backgroundColor: "white", fontWeight: "bolder", border: "solid red 2px" }}>
