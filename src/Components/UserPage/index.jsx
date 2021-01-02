7defc53c449d39cad9b7401f1050b445de2efae3
Alexander <alexander7-93@yandex.ru>
2021-01-02 14:51:55 +0300

    исправил ссылки на группы на страницах других пользователей

--- src/Components/UserPage/index.jsx
+++ src/Components/UserPage/index.jsx
@@ -17,43 +17,43 @@ const UserPage = () => {
   if (id === curentUser) { history.push(`/Home/${curentUser}`) }
 
 
-	const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     (() => {
       setTimeout(() => {
-      dispatch(LoadUserPage(id))
-    }, 500);
+        dispatch(LoadUserPage(id))
+      }, 500);
     })()
-	}, [])
-	
+  }, [])
+
   const foto = 'https://pl4324260.e-naturessunshine.com/images/img-profile.png'
   const userInfo = useSelector(state => state.userPage)
-// console.log(userInfo);
+  // console.log(userInfo);
   return (
     <div className="userMainBlock">
-		{ userInfo[0] ? 
-      <div className="userWrap">
-        <div className="leftColumn">
+      { userInfo[0] ?
+        <div className="userWrap">
+          <div className="leftColumn">
 
-        {/* <div className="fotoBlock"><img src={userInfo[0].img ? `/userPic/${userInfo[0].img}` : foto} alt="userPhoto" /></div> */}
-        {<div style={(userInfo[0].img && { background: `url(/userPic/${userInfo[0].img})` }) ? { background: `url(/userPic/${userInfo[0].img})` } : { background: `url(${foto})` }} className="userAvatar" />}
-          <div className="userName">
-            {
-              userInfo.length &&
-              <h1><span className="yellowSymbols">{'//'}{' '}</span>{userInfo[0].firstname}<br/>{userInfo[0].surname}<span className="yellowSymbols">{' '}{'//'}</span></h1>
-            }
+            {/* <div className="fotoBlock"><img src={userInfo[0].img ? `/userPic/${userInfo[0].img}` : foto} alt="userPhoto" /></div> */}
+            {<div style={(userInfo[0].img && { background: `url(/userPic/${userInfo[0].img})` }) ? { background: `url(/userPic/${userInfo[0].img})` } : { background: `url(${foto})` }} className="userAvatar" />}
+            <div className="userName">
+              {
+                userInfo.length &&
+                <h1><span className="yellowSymbols">{'//'}{' '}</span>{userInfo[0].firstname}<br />{userInfo[0].surname}<span className="yellowSymbols">{' '}{'//'}</span></h1>
+              }
+            </div>
+            <div className="userMenuBlock"><UserMenuUserPage userInfo={userInfo} /></div>
           </div>
-          <div className="userMenuBlock"><UserMenuUserPage userInfo={userInfo}/></div>
-        </div>
 
-        <div className="rightColumn">
+          <div className="rightColumn">
 
-          <div className="wallBlock"><Wall /></div>
-        </div>
-      </div> : 
-			<Loader/> 
-			}
+            <div className="wallBlock"><Wall /></div>
+          </div>
+        </div> :
+        <Loader />
+      }
     </div>
   );
 }
