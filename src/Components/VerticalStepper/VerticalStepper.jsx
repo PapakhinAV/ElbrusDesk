164e26a2feaaca1d4e48900b9dcbdb6bbf2aaa8a
Alexander <alexander7-93@yandex.ru>
2021-01-11 21:02:49 +0300

    обновлена презентация

--- src/Components/VerticalStepper/VerticalStepper.jsx
+++ src/Components/VerticalStepper/VerticalStepper.jsx
@@ -1,130 +1,29 @@
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Stepper from '@material-ui/core/Stepper';
-// import Step from '@material-ui/core/Step';
-// import StepLabel from '@material-ui/core/StepLabel';
-// import StepContent from '@material-ui/core/StepContent';
-// import Button from '@material-ui/core/Button';
-// import Paper from '@material-ui/core/Paper';
-// import Typography from '@material-ui/core/Typography';
-// import odin from '../img/profile/1.jpg'
-// import dva from '../img/profile/2.jpg'
-// import tri from '../img/profile/3.jpg'
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     width: '100%',
-//   },
-//   button: {
-//     marginTop: theme.spacing(1),
-//     marginRight: theme.spacing(1),
-//   },
-//   actionsContainer: {
-//     marginBottom: theme.spacing(2),
-//   },
-//   resetContainer: {
-//     padding: theme.spacing(3),
-//   },
-// }));
-
-// function getSteps() {
-//   return ['Главная', 'О приложении', 'Возможности'];
-// }
-
-// function getStepContent(step) {
-//   switch (step) {
-//     case 0:
-//       return <img className="slide" src ={odin} alt="slide1" />;
-//     case 1:
-//       return <img className="slide" src ={dva} alt="slide2" />;
-//     case 2:
-//       return <img className="slide" src ={tri} alt="slide3" />;;
-//     default:
-//       return 'Unknown step';
-//   }
-// }
-
-// export default function VerticalLinearStepper() {
-//   const classes = useStyles();
-//   const [activeStep, setActiveStep] = React.useState(0);
-//   const steps = getSteps();
-
-//   const handleNext = () => {
-//     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-//   };
-
-//   const handleBack = () => {
-//     setActiveStep((prevActiveStep) => prevActiveStep - 1);
-//   };
-
-//   const handleReset = () => {
-//     setActiveStep(0);
-//   };
-
-//   return (
-//     <div className={classes.root}>
-//       <Stepper activeStep={activeStep} orientation="vertical">
-//         {steps.map((label, index) => (
-//           <Step key={label}>
-//             <StepLabel>{label}</StepLabel>
-//             <StepContent>
-//               <Typography>{getStepContent(index)}</Typography>
-//               <div className={classes.actionsContainer}>
-//                 <div>
-//                   <Button
-//                     disabled={activeStep === 0}
-//                     onClick={handleBack}
-//                     className={classes.button}
-//                   >
-//                     Вернуться
-//                   </Button>
-//                   <Button
-//                     variant="contained"
-//                     color="primary"
-//                     onClick={handleNext}
-//                     className={classes.button}
-//                   >
-//                     {activeStep === steps.length - 1 ? 'Конец' : 'Вперед'}
-//                   </Button>
-//                 </div>
-//               </div>
-//             </StepContent>
-//           </Step>
-//         ))}
-//       </Stepper>
-//       {activeStep === steps.length && (
-//         <Paper square elevation={0} className={classes.resetContainer}>
-//           <Typography>Спасибо за внимание!</Typography>
-//           <Button onClick={handleReset} className={classes.button}>
-//             Сбросиить
-//           </Button>
-//         </Paper>
-//       )}
-//     </div>
-//   );
-// }
-import React, { memo } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import odin from '../img/profile/1.jpg'
 import dva from '../img/profile/2.jpg'
 import tri from '../img/profile/3.jpg'
-import chetiri from '../img/profile/4.jpg'
-import pyat from '../img/profile/5.jpg'
+// import chetiri from '../img/profile/4.jpg'
 import shest from '../img/profile/6.jpg'
 import sem from '../img/profile/7.jpg'
 import finish from '../img/profile/finish.jpg'
+
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepButton from '@material-ui/core/StepButton';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
   },
-  backButton: {
+  button: {
     marginRight: theme.spacing(1),
+  },
+  completed: {
+    display: 'inline-block',
   },
   instructions: {
     marginTop: theme.spacing(1),
@@ -133,84 +32,115 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function getSteps() {
-  return ['Главная', 'О проекте', 'Возможности', 'Трудности', 'Технологии', 'Планы'];
-
+  return ['Главная', 'О проекте', 'Возможности', 'Технологии', 'Планы'];
 }
 
-function getStepContent(stepIndex) {
-  switch (stepIndex) {
+function getStepContent(step) {
+  switch (step) {
     case 0:
-      return <img className="slide" src ={odin} alt="slide1" />;
+      return <img className="slide" src={odin} alt="slide1" />;
     case 1:
-      return <img className="slide" src ={dva} alt="slide2" />;
+      return <img className="slide" src={dva} alt="slide2" />;
     case 2:
-      return <img className="slide" src ={tri} alt="slide3" />;
-      case 3:
-      return <img className="slide" src ={chetiri} alt="slide4" />;
+      return <img className="slide" src={tri} alt="slide3" />;
+    case 3:
+      return <img className="slide" src={shest} alt="slide6" />;
     case 4:
-
-      return <img className="slide" src ={shest} alt="slide6" />;
-    case 5:
-
-      return <img className="slide" src ={sem} alt="slide7" />;
+      return <img className="slide" src={sem} alt="slide7" />;
+    // case 5:
+    //   return <img className="slide" src={sem} alt="slide7" />;
     default:
       return 'Unknown stepIndex';
   }
 }
 
-function HorizontalLabelPositionBelowStepper() {
+export default function HorizontalNonLinearStepper() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
+  const [completed, setCompleted] = React.useState({});
   const steps = getSteps();
 
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
   const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+        // find the first step that has been completed
+        steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
   };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
+  };
+
   const handleReset = () => {
     setActiveStep(0);
+    setCompleted({});
   };
 
   return (
     <div className={classes.root}>
-      <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map((label) => (
+      <Stepper nonLinear activeStep={activeStep}>
+        {steps.map((label, index) => (
           <Step key={label}>
-            <StepLabel>{label}</StepLabel>
+            <StepButton onClick={handleStep(index)} completed={completed[index]}>
+              {label}
+            </StepButton>
           </Step>
         ))}
       </Stepper>
       <div>
         {activeStep === steps.length ? (
           <div className="projectButtons">
-            <Typography className={classes.instructions}><img className="slide" src ={finish} alt="finish" /></Typography>
+            <Typography className={classes.instructions}><img className="slide" src={finish} alt="finish" /></Typography>
             <Button className="yellowButton" onClick={handleReset}>Сбросить</Button>
           </div>
         ) : (
-          <div>
-            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-            <div className="projectButtons">
-              <Button
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                className={classes.backButton}
-              >
-                Назад
+            <div>
+              <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+              <div className="projectButtons">
+                <Button
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  className={classes.backButton}
+                >
+                  Назад
               </Button>
-              <Button className="purpleButton" variant="contained" color="primary" onClick={handleNext}>
-                {activeStep === steps.length - 1 ? 'Конец' : 'Далее'}
-              </Button>
+                <Button className="purpleButton" variant="contained" color="primary" onClick={handleComplete}>
+                  {activeStep === steps.length - 1 ? 'Конец' : 'Далее'}
+                </Button>
+              </div>
             </div>
-          </div>
-        )}
+          )}
       </div>
     </div>
   );
 }
-
-export default memo(HorizontalLabelPositionBelowStepper)
-
